f89053a0ab034384ebf10a2960e4ae12ecbef06f
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-01-24 23:27:40 +0400

    create js

---
+++ javascript/app.js
@@ -0,0 +1,35 @@
+console.log("test")
+
+let firstName = "Nikolo"
+let lastName = "Dzaniolo"
+console.log(firstName + " " + lastName)
+
+let x = 8;
+let y = 3;
+let z = 2;
+let b = "8"
+let sum = x + y; 
+let div = x / z;
+let mult = y * z;
+let divWithPersent = x % y; 
+let isCheked = false;
+
+console.log(x+y);
+console.log(sum);
+console.log(div);
+console.log(mult);
+console.log(divWithPersent);
+console.log(isCheked);
+console.log(typeof isCheked);
+console.log(x === b)
+
+
+let t = 16
+let l = 38
+
+console.log (t * l)
+
+
+const r = 7
+
+console.log (r * r * 3.14)
